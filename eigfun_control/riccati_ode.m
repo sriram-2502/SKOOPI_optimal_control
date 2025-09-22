f55f5d3b35034d54b91f230edbc8d534cf543515
Sriram Krishnamoorthy <sriramk@clemson.edu>
2025-09-22 19:23:33 -0400

    updated setup

--- eigfun_control/riccati_ode.m
+++
@@ -1,6 +0,0 @@
-function P_dot = riccati_ode(t,P,A,B,Q,R)
-    % function to output Differetial Ricciati ODE 
-    P = reshape(P,size(A));
-    P_dot = -(A'*P + P*A - P*B*inv(R)*B'*P + Q);
-    P_dot = P_dot(:); % vectorize for ode solving
-end
--- utils/saturate_fun.m
+++
@@ -1,5 +0,0 @@
-function saturation =  saturate_fun(x, x_max, x_min) 
-
-    saturation = min(x_max, max(x_min,x)) ; % Saturation Function
-
-end
